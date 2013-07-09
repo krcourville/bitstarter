fc0134efcce25df74c9eca67262067c62d5769a2
Ken Courville <krcourville@gmail.com>
2013-07-08 20:36:20 -0600

    response comes from index.html now

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+var buf = require('buffer');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffer = fs.readFileSync('index.html');
+  var html = buffer.toString();
+  response.send(html);
 });
 
 var port = process.env.PORT || 5000;
